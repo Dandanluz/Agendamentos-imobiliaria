ae54e090c25ff822362cbf07fc318ba582b00ad0
ester-lucena <esterdelucenapaiva@gmail.com>
2025-07-14 10:22:36 -0300

    Update imovel.h

--- imovel.h
+++ imovel.h
@@ -1,21 +1,30 @@
 #ifndef IMOVEL_H
 #define IMOVEL_H
-#include <iostream>
 #include <string>
-using namespace std;
-
-enum TipoImovel{Casa, Apartamento, Terreno};
 
 class Imovel{
-    public:
+private:
     static int nextId;
     int id;
-    TipoImovel tipo;
-    int ProprietarioID;
-    double lat, lon, preço;
-    string endereço;
+    int proprietario_id;
+    std::string tipo;
+    double lat;
+    double lgt;
+    std::string endereço;
+    double preço;
 
-    Imovel(TipoImovel tipo, int ProprietarioID, double lat, double lon, const string &endereço, double preço);
+public:
+    Imovel();
+    void settipo(std::string tipo);
+    void setproprietario_id(int prorpietario_id);
+    void setlat(double lat2);
+    double getlat();
+    void setlgt(double lgt2);
+    double getlgt();
+    void setendereço(std::string endereço);
+    void setpreço(double preço);
+    void exibirinfo();
+    int getid();
 };
 
 #endif
