da0d62e41e570b87780d32909236f3490d9c9f16
ester-lucena <esterdelucenapaiva@gmail.com>
2025-07-14 10:23:17 -0300

    Update imovel.cpp

--- imovel.cpp
+++ imovel.cpp
@@ -1,10 +1,55 @@
-#include <iostream>
 #include "imovel.h"
-
+#include <iostream>
 using namespace std;
 
-int Imovel:: nextId = 1;
-Imovel::Imovel(TipoImovel tipo, int ProprietarioID, double lat, double lon, const string &endereço, double preço)
-    : tipo(tipo), ProprietarioID(ProprietarioID), lat(lat), lon(lon), endereço(endereço), preço(preço){
-        id = nextId++;
-    }
+int Imovel::nextId = 1;
+
+Imovel::Imovel(){
+    id = nextId++;
+}
+
+void Imovel::settipo(string tipo){
+    this->tipo = tipo;
+}
+
+void Imovel::setproprietario_id(int proprietario_id){
+    this->proprietario_id = proprietario_id;
+}
+
+void Imovel::setlat(double lat2){
+    lat = lat2;
+}
+
+double Imovel::getlat(){
+    return lat;
+}
+
+void Imovel::setlgt(double lgt2){
+    lgt = lgt2;
+}
+
+double Imovel::getlgt(){
+    return lgt;
+}
+
+void Imovel::setendereço(string endereço){
+    this->endereço = endereço;
+}
+
+void Imovel::setpreço(double preço){
+    this->preço = preço;
+}
+
+int Imovel::getid(){
+    return id;
+}
+
+void Imovel::exibirinfo(){
+    cout << "tipo: " << tipo << endl;
+    cout << "proprietario_id: " << proprietario_id << endl;
+    cout << "lat: " << lat << endl;
+    cout << "lgt: " << lgt << endl;
+    cout << "endereço: " << endereço << endl;
+    cout << "preço: " << preço << endl;
+    cout << "id: " << id << endl;
+}
