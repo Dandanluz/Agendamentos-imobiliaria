4b22c9f42fafde7c5b9ca9bd252a74d3dc81e5e7
ester-lucena <esterdelucenapaiva@gmail.com>
2025-07-14 10:21:46 -0300

    Update cliente.cpp

--- cliente.cpp
+++ cliente.cpp
@@ -1,10 +1,23 @@
-#include <iostream>
 #include "cliente.h"
-
+#include <iostream>
 using namespace std;
 
-int Cliente:: nextId = 1;
-Cliente::Cliente(const std::string &telefone, const std::string &nome)
-    : telefone(telefone), nome(nome){
-        id = nextId++;
-    }
+int Cliente::nextId = 1;
+
+Cliente::Cliente(){
+    id = nextId++;
+}
+
+void Cliente::setnome(string nome2){
+    nome = nome2;
+}
+
+void Cliente::settelefone(string telefone2){
+    telefone = telefone2;
+}
+
+void Cliente::exibirinfo(){
+    cout << "nome: " << nome << endl;
+    cout << "telefone: " << telefone << endl;
+    cout << "id: " << id << endl;
+}
